6d496406bf3a0621c411aaae7a462ce4e4bf5de4
kysaeed <kysaeed@gmail.com>
2018-11-25 16:29:38 +0900

    TTFのパラメータ名をスネークケースに修正

--- font-ob/app/TtfHead.php
+++ font-ob/app/TtfHead.php
@@ -9,75 +9,35 @@ class TtfHead extends Model
 	use TraitTtfFileElement;
 
 	const FileFormat = [
-		'majorVersion' => ['n', 1],
-		'minorVersion' => ['n', 1],
-		'fontRevision' => ['N', 1],
-		'checkSumAdjustment' => ['N', 1],
-		'magicNumber' => ['N', 1],
+		'major_version' => ['n', 1],
+		'minor_version' => ['n', 1],
+		'font_revision' => ['N', 1],
+		'check_sum_adjustment' => ['N', 1],
+		'magic_number' => ['N', 1],
 		'flags' => ['n', 1],
-		'unitsPerEm' => ['n', 1],
+		'units_per_em' => ['n', 1],
 		'created' => ['J', 1],
 		'modified' => ['J', 1],
-		'xMin' => ['n', 1, true],
-		'yMin' => ['n', 1, true],
-		'xMax' => ['n', 1, true],
-		'yMax' => ['n', 1, true],
-		'macStyle' => ['n', 1],
-		'lowestRecPPEM' => ['n', 1],
-		'fontDirectionHint' => ['n', 1, true],
-		'indexToLocFormat' => ['n', 1, true],
-		'glyphDataFormat' => ['n', 1, true],
+		'x_min' => ['n', 1, true],
+		'y_min' => ['n', 1, true],
+		'x_max' => ['n', 1, true],
+		'y_max' => ['n', 1, true],
+		'mac_style' => ['n', 1],
+		'lowest_rec_ppem' => ['n', 1],
+		'font_direction_hint' => ['n', 1, true],
+		'index_to_loc_format' => ['n', 1, true],
+		'glyph_data_format' => ['n', 1, true],
 	];
 
 	public static function createFromFile($binTtfFile, $offset)
 	{
 		$data = self::unpackBinData(self::FileFormat, $binTtfFile, $offset);
-
-		return new TtfHead([
-			'major_version'=> $data['majorVersion'],
-			'minor_version'=> $data['minorVersion'],
-			'font_revision'=> $data['fontRevision'],
-			'check_sum_adjustment'=> $data['checkSumAdjustment'],
-			'magic_number'=> $data['magicNumber'],
-			'flags'=> $data['flags'],
-			'units_per_em'=> $data['unitsPerEm'],
-			'created'=> $data['created'],
-			'modified'=> $data['modified'],
-			'x_min'=> $data['xMin'],
-			'y_min'=> $data['yMin'],
-			'x_max'=> $data['xMax'],
-			'y_max'=> $data['yMax'],
-			'mac_style'=> $data['macStyle'],
-			'lowest_rec_ppem'=> $data['lowestRecPPEM'],
-			'font_direction_hint'=> $data['fontDirectionHint'],
-			'index_to_loc_format'=> $data['indexToLocFormat'],
-			'glyph_data_format'=> $data['glyphDataFormat'],	// TODO: 要確認!!
-		]);
+		return new TtfHead($data);
 	}
 
 	public function toBinary()
 	{
-
-		return self::packAttributes(self::FileFormat, [
-			'majorVersion' => $this->major_version,
-			'minorVersion' => $this->minor_version,
-			'fontRevision' => $this->font_revision,
-			'checkSumAdjustment' => $this->check_sum_adjustment,
-			'magicNumber' => $this->magic_number,
-			'flags' => $this->flags,
-			'unitsPerEm' => $this->units_per_em,
-			'created' => $this->created,
-			'modified' => $this->modified,
-			'xMin' => $this->x_min,
-			'yMin' => $this->y_min,
-			'xMax' => $this->x_max,
-			'yMax' => $this->y_max,
-			'macStyle' => $this->mac_style,
-			'lowestRecPPEM' => $this->lowest_rec_ppem,
-			'fontDirectionHint' => $this->font_directionHint,
-			'indexToLocFormat' => $this->index_to_loc_format,
-			'glyphDataFormat' => $this->glyph_data_format,
-		]);
+		return self::packAttributes(self::FileFormat, $this->getAttributes());
 	}
 
 	protected $fillable = [
@@ -96,8 +56,8 @@ class TtfHead extends Model
 		'y_max',
 		'mac_style',
 		'lowest_rec_ppem',
-		'font_directionHint',
+		'font_direction_hint',
 		'index_to_loc_format',
-		'glyph_data_format',	// TODO: 要確認!!
+		'glyph_data_format',	// TODO: フォーマットの種別を確認!!
 	];
 }
